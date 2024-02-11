b0c2701060011de79a3321bac1d30f89cd767d50
Karthik Raja Selvan <101384002+KarthikRaja-Selvan@users.noreply.github.com>
2024-02-12 03:30:53 +0800

    commit

--- _next/static/chunks/app/page-a53c2c10bc9b282e.js
+++ _next/static/chunks/app/page-a53c2c10bc9b282e.js
@@ -59,7 +59,7 @@
                             ,
                             className: " bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded",
                             children: 0 === e ? "No" : (()=>{
-                                let t = ["No", "Are you sure?", "Really sure?", "Are you positive???", "Pookie please", "Just think about it", "If you say no, I'll be very sad", "I'll be very very sad", "I'll be very very very sad", "I'll be very very very very sad", "Ok fine, I'll stop asking...", "Just kidding, PLEASE SAY YES", "I'll be very very very very very sad", "You're breaking my heart ;("];
+                                let t = ["No", "Are you sure?", "Really sure?", "Are you positive???", "Pookie please", "Just think about it", "If you say no, I'll be very sad", "I'll be very very sad", "I'll be very very very sad", "I'll be very very very very sad", "Ok fine, I'll stop asking...", "Just kidding, PLEASE SAY YES", "I'll be very very very very very sad", "You're breaking my heart ;(", "OIII", "Yov"];
                                 return t[e % t.length]
                             }
                             )()
